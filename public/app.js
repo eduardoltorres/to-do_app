36db84f01d5dd849ddcdf1518e71a4aa8cb8e9eb
Eduardo L. Torres <eltorrescuesta@gmail.com>
2018-11-23 09:00:35 +0100

    connect form to api

--- public/app.js
+++ public/app.js
@@ -4,17 +4,33 @@ $(document).ready(function(){
 
   $('#todoInput').keypress(function(event){
     if(event.which == 13) {
-      createTodo()
+      createTodo();
     }
   });
 });
 
 function addTodos(todos){
   todos.forEach(function(todo){
-    var newTodo = $('<li class="task">'+ todo.name + '</li>');
-    if(todo.completed){
-      newTodo.addClass('done');
-    }
-    $('.list').append(newTodo);
+    addTodo(todo);
+  });
+}
+
+function addTodo(todo){
+  var newTodo = $('<li class="task">'+ todo.name + '</li>');
+  if(todo.completed){
+    newTodo.addClass('done');
+  }
+  $('.list').append(newTodo);
+}
+
+function createTodo(){
+  var usrInput = $('#todoInput').val();
+  $.post('/api/todos', {name: usrInput})
+  .then(function(newTodo){
+    $('#todoInput').val('');
+    addTodo(newTodo);
+  })
+  .catch(function(err){
+    console.log(err);
   });
 }
