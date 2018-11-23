9722a07eb9436043a57a907b7ef31f8d8c74af80
Eduardo L. Torres <eltorrescuesta@gmail.com>
2018-11-23 07:25:55 +0100

    finish routes and refactor

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var express = require("express"),
-    app     = express();
-    port    = 3000;
+    app     = express(),
+    port    = 3000,
+    bodyParser = require('body-parser');
+
+var todoRoutes = require('./routes/todos')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.send("HI THERE FROM EXPRESS!!!")
+  res.send("HI THERE FROM THE ROOT ROUTE")
 });
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, function(){
   console.log("APP IS RUNNING");
