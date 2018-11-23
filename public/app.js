9511bd3d9535d7d8ee4c60d8afe3b04b50a88efd
Eduardo L. Torres <eltorrescuesta@gmail.com>
2018-11-23 08:45:07 +0100

    display todos correctly; add done class

--- public/app.js
+++ public/app.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
   $.getJSON('/api/todos')
   .then(addTodos)
+
+  $('#todoInput').keypress(function(event){
+    if(event.which == 13) {
+      createTodo()
+    }
+  });
 });
 
 function addTodos(todos){
   todos.forEach(function(todo){
     var newTodo = $('<li class="task">'+ todo.name + '</li>');
+    if(todo.completed){
+      newTodo.addClass('done');
+    }
     $('.list').append(newTodo);
-  })
+  });
 }
