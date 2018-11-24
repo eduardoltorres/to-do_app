f69284722951d429d57e452099975d629eb65e5a
Eduardo L. Torres <eltorrescuesta@gmail.com>
2018-11-24 06:35:26 +0100

    add delete route

--- public/app.js
+++ public/app.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
       createTodo();
     }
   });
+
+  $('.list').on('click', 'span', function(){
+    removeTodo($(this).parent());
+  })
 });
 
 function addTodos(todos){
@@ -16,7 +20,8 @@ function addTodos(todos){
 }
 
 function addTodo(todo){
-  var newTodo = $('<li class="task">'+ todo.name + '</li>');
+  var newTodo = $('<li class="task">'+ todo.name + '<span>X</span></li>');
+  newTodo.data('id', todo._id);
   if(todo.completed){
     newTodo.addClass('done');
   }
@@ -33,4 +38,19 @@ function createTodo(){
   .catch(function(err){
     console.log(err);
   });
+}
+
+function removeTodo(todo){
+  var clickedId = todo.data('id');
+  var deleteUrl = '/api/todos/' + clickedId;
+  $.ajax({
+    method: 'DELETE',
+    url: deleteUrl
+  })
+  .then(function(data){
+    todo.remove();
+  })
+  .catch(function(err){
+    console.log(err);
+  })
 }
